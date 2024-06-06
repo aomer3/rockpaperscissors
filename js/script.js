873e4fdb79986eaece04dc7d6c6f561074c5a39a
aomer3 <67030297+aomer3@users.noreply.github.com>
2024-06-06 19:17:43 -0400

    Add UI to display running score and final score
    Add buttons and UI and remove console.logs to interact with game through UI instead of console.logs

--- js/script.js
+++ js/script.js
@@ -26,82 +26,102 @@ function getComputerChoiceUpperCase(result){
 
 //Human selects rock, paper or scissors
 function getHumanChoice() {
-    let result = prompt("Please enter rock, paper or scissors:");
-    let choice = getHumanChoiceUpperCase(result);
-    return choice.toUpperCase();
+    let selectedButton = "";
+    const buttons = document.querySelectorAll(".buttons");
+    
+    buttons.forEach(button => {
+        button.addEventListener("click", () => {
+            if(button.id === 'button-rock'){
+                selectedButton = "ROCK";
+                playRound(selectedButton);
+            }
+            else if(button.id === 'button-paper'){
+                selectedButton = "PAPER";
+                playRound(selectedButton);
+            }
+            else if(button.id === 'button-scissors') {
+                selectedButton = "SCISSORS";
+                playRound(selectedButton);
+            }
+
+        });
+
+    });
+
 }
 
-function getHumanChoiceUpperCase(result){
-    switch (result) {
-        case result.toUpperCase() === "ROCK":
-            result = "ROCK";
-            break;
-        case result.toUpperCase() === "PAPER":
-            result = "PAPER";
-            break;
-        case result.toUpperCase() === "SCISSORS":
-            result = "SCISSORS";
-            break;
-        default:
-            break;
-    }
-    return result;
-}
+
 
 //Record score of played game
 let humanScore = 0;
 let computerScore = 0;
 
-function playRound(){
+function playRound(selectedButton){
     const computerChoice = getComputerChoice();
-    const humanChoice = getHumanChoice();
+    const humanChoice = selectedButton;
 
-    switch (true) {
-        case computerChoice === "ROCK" && humanChoice === "ROCK":
-            break;
-        case computerChoice === "ROCK" && humanChoice === "PAPER":
-            humanScore++;
-            console.log("You win! Paper beats Rock!");
-            break;
-        case computerChoice === "ROCK" && humanChoice === "SCISSORS":
-            computerScore++;
-            console.log("You lose! Rock beats Scissors!");
-            break;
-        case computerChoice === "PAPER" && humanChoice === "ROCK":
-            computerScore++;
-            console.log("You lose! Paper beats Rock!");
-            break;
-        case computerChoice === "PAPER" && humanChoice === "PAPER":
-            break;
-        case computerChoice === "PAPER" && humanChoice === "SCISSORS":
-            humanScore++;
-            console.log("You win! Scissors beats Paper!");
-            break;
-        case computerChoice === "SCISSORS" && humanChoice === "ROCK":
-            humanScore++;
-            console.log("You win! Rock beats Scissors!");
-            break;
-        case computerChoice === "SCISSORS" && humanChoice === "PAPER":
-            computerScore++;
-            console.log("You lose! Scissors beats Paper!");
-            break;
-        case computerChoice === "SCISSORS" && humanChoice === "SCISSORS":
-            break;
-        default:
-            break;
+    let body = document.body;
+        
+    let divScores = document.createElement("div");
+    
+    let divHeader = document.createElement("h1");
+    divHeader.textContent = "Final scores:";
+
+    let paraHuman = document.createElement("p");
+    paraHuman.textContent = `Human Score: ${humanScore}`;
+    let paraComputer = document.createElement("p");
+    paraComputer.textContent = `Computer Score: ${computerScore}`;
+
+    if(humanScore < 5 && computerScore < 5){
+        switch (true) {
+            case computerChoice === "ROCK" && humanChoice === "ROCK":
+                break;
+            case computerChoice === "ROCK" && humanChoice === "PAPER":
+                humanScore++;
+                break;
+            case computerChoice === "ROCK" && humanChoice === "SCISSORS":
+                computerScore++;
+                break;
+            case computerChoice === "PAPER" && humanChoice === "ROCK":
+                computerScore++;
+                break;
+            case computerChoice === "PAPER" && humanChoice === "PAPER":
+                break;
+            case computerChoice === "PAPER" && humanChoice === "SCISSORS":
+                humanScore++;
+                break;
+            case computerChoice === "SCISSORS" && humanChoice === "ROCK":
+                humanScore++;
+                break;
+            case computerChoice === "SCISSORS" && humanChoice === "PAPER":
+                computerScore++;
+                break;
+            case computerChoice === "SCISSORS" && humanChoice === "SCISSORS":
+                break;
+            default:
+                break;
+        }
+        
+        divScores.append(divHeader, paraHuman, paraComputer);
+        body.append(divScores);
+
     }
+    else if (humanScore === 5){
+        let humanWins = document.createElement("h2");
+        humanWins.textContent = "You Wins!";
+        divScores.append(divHeader, humanWins, paraHuman, paraComputer);
+        body.append(divScores);
+    }
+    else if (computerScore === 5){
+        let compterWins = document.createElement("h2");
+        compterWins.textContent = "Computer Wins!";
+        divScores.append(divHeader, compterWins, paraHuman, paraComputer);
+        body.append(divScores);
+    }
+
+
 }
 
-function playGame(){
-    let games = 5;
-    for (let index = 1; index <= games; index++) {
-        playRound();
-    }
-}
+getHumanChoice();
 
-playGame();
-
-console.log(`Final scores:
-             Human Score: ${humanScore}
-             Computer SCore: ${computerScore}`);
 
