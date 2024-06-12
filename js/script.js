0889289ece53f7f9455333c8241b8c1198b074c2
aomer3 <67030297+aomer3@users.noreply.github.com>
2024-06-11 21:51:42 -0400

    Add reset button, styling, images, fix div repeats
    Add reset button and score count resetting, add styling to header and buttons, add rock paper scissor images, fix issue with onClick div repeating on page instead of overwriting previous div

--- js/script.js
+++ js/script.js
@@ -27,24 +27,53 @@ function getComputerChoiceUpperCase(result){
 //Human selects rock, paper or scissors
 function getHumanChoice() {
     let selectedButton = "";
+    let round = 0;
     const buttons = document.querySelectorAll(".buttons");
     
     buttons.forEach(button => {
         button.addEventListener("click", () => {
             if(button.id === 'button-rock'){
+                round++;
                 selectedButton = "ROCK";
-                playRound(selectedButton);
+                playRound(selectedButton, round);
             }
             else if(button.id === 'button-paper'){
+                round++;
                 selectedButton = "PAPER";
-                playRound(selectedButton);
+                playRound(selectedButton, round);
             }
             else if(button.id === 'button-scissors') {
+                round++;
                 selectedButton = "SCISSORS";
+                playRound(selectedButton, round);
+            }
+            else if(button.id === "button-reset"){
+                selectedButton = "RESET";
                 playRound(selectedButton);
             }
 
         });
+
+        button.addEventListener("mouseenter", () => {
+            button.style.backgroundColor = "greenyellow";
+        });
+
+        button.addEventListener("mouseleave", () => {
+            if(button.id === "button-rock"){
+                button.style.backgroundColor = "red";
+            }
+            else if(button.id === "button-paper"){
+                button.style.backgroundColor = "cyan";
+            }
+            else if(button.id === "button-scissors"){
+                button.style.backgroundColor = "yellow";
+            }
+            else if(button.id === "button-reset"){
+                round = 0;
+                button.style.backgroundColor = "orange";
+            }
+        });
+
 
     });
 
@@ -56,21 +85,69 @@ function getHumanChoice() {
 let humanScore = 0;
 let computerScore = 0;
 
-function playRound(selectedButton){
+function playRound(selectedButton, round){    
+    let divHeader = document.createElement("h1");
+    let paraHuman = document.createElement("p");
+    let paraComputer = document.createElement("p");
+    let divScores = document.createElement("div");
+    let imgHuman = document.createElement("img");
+    let imgComputer = document.createElement("img");
+
+    const buttons = document.querySelectorAll(".buttons");
+
+    buttons.forEach(button => {
+        button.addEventListener("click", () => {
+            divScores.textContent = "";
+        })
+
+    });
+
     const computerChoice = getComputerChoice();
     const humanChoice = selectedButton;
 
     let body = document.body;
-        
-    let divScores = document.createElement("div");
-    
-    let divHeader = document.createElement("h1");
-    divHeader.textContent = "Final scores:";
 
-    let paraHuman = document.createElement("p");
-    paraHuman.textContent = `Human Score: ${humanScore}`;
-    let paraComputer = document.createElement("p");
-    paraComputer.textContent = `Computer Score: ${computerScore}`;
+    
+    
+    divHeader.textContent = `Round ${round}`;
+    divHeader.style.textAlign = "center";
+    divHeader.style.margin = "10px";
+
+    paraHuman.textContent = `You: ${humanScore}`;
+    paraHuman.style.textAlign = "center";
+    paraHuman.style.margin = "10px";
+
+    paraComputer.textContent = `Computer: ${computerScore}`;
+    paraComputer.style.textAlign = "center";
+    paraComputer.style.margin = "10px";
+
+    if(selectedButton === "ROCK"){
+        imgHuman.src = "./img/rock.png";
+    }
+    else if (selectedButton === "PAPER"){
+        imgHuman.src = "./img/paper.png";
+    }
+    else if (selectedButton === "SCISSORS"){
+        imgHuman.src = "./img/scissors.png";
+    }
+
+
+    if(computerChoice === "ROCK"){
+        imgComputer.src = "./img/rock.png";
+    }
+    else if (computerChoice === "PAPER"){
+        imgComputer.src = "./img/paper.png";
+    }
+    else if (computerChoice === "SCISSORS"){
+        imgComputer.src = "./img/scissors.png";
+    }
+
+    imgHuman.style.width = "100px";
+    imgHuman.style.margin = "20px";
+    imgComputer.style.width = "100px";
+    imgComputer.style.margin = "20px";
+
+
 
     if(humanScore < 5 && computerScore < 5){
         switch (true) {
@@ -102,25 +179,37 @@ function playRound(selectedButton){
                 break;
         }
         
-        divScores.append(divHeader, paraHuman, paraComputer);
+        divScores.append(imgHuman, imgComputer, divHeader, paraHuman, paraComputer);
         body.append(divScores);
-
     }
     else if (humanScore === 5){
         let humanWins = document.createElement("h2");
-        humanWins.textContent = "You Wins!";
+        humanWins.textContent = "You Win!";
+        humanWins.style.textAlign = "center";
+        humanWins.style.color = "red";
+        humanWins.style.fontWeight = "bold";
         divScores.append(divHeader, humanWins, paraHuman, paraComputer);
         body.append(divScores);
     }
     else if (computerScore === 5){
         let compterWins = document.createElement("h2");
         compterWins.textContent = "Computer Wins!";
+        compterWins.style.textAlign = "center";
+        compterWins.style.color = "red";
+        compterWins.style.fontWeight = "bold";
         divScores.append(divHeader, compterWins, paraHuman, paraComputer);
         body.append(divScores);
     }
 
+    if (selectedButton === "RESET"){
+        humanScore = 0;
+        computerScore = 0;
+        divScores.remove(imgHuman, imgComputer);
+        body.divScores(divScores, newGame);
+    }
 
-}
+};
+
 
 getHumanChoice();
 
